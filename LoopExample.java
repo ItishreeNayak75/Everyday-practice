5a6eb61c58cc6f4c80ad24b04d2cfcef89f8f490
Itishree Nayak <nayakitishree758@gmail.com>
2025-07-27 12:58:29 +0530

    Add files via upload

---
+++ LoopExample.java
@@ -0,0 +1,18 @@
+public class LoopExample {
+    public static void main(String[] args) {
+        
+      
+        System.out.println("Using for loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        
+        System.out.println("Using while loop:");
+        int j = 6;
+        while (j <= 10) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
